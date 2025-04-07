13404ebb940db56ff7a6d6ef9a61cdbccb35fcd0
Alejandro Restrepo Klinge <56825548+mrrestre@users.noreply.github.com>
2025-04-07 14:35:46 +0200

    Initial commit

---
+++ src/main/java/com/code/interview/digital_assistant/DigitalAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.code.interview.digital_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/interview/digital_assistant/DigitalAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.interview.digital_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
